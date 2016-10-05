6dcc5057cd8ed2ef595eaf6c41fd3f8210acb65b
Isaac <isaacdura@gmail.com>
2016-10-05 19:56:07 +0200

    Enum containing the articles

---
+++ src/main/java/com/mycompany/basicshopjava/Articles.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.basicShopjava;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+
+/**
+ *
+ * @author Isaac
+ */
+public enum Articles {
+
+    SILK, BOBINET, BURLAP, BENARAS, BATIK, BATISTE, JUTE, JERSEY, KNIT;
+}
+
+
+
+
+
+
+  
+    
+   
+
+
+
+
+
